75dd6e5fd5ef32a03fd40bd8359ff69add4edf5a
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-09-23 17:07:09 -0600

    homepage pulls all the posts from the db and renders them

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,26 @@
 const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
+//Get all the posts to display on homepage
 router.get("/", async (req, res) => {
   try {
+    const data = await Post.findAll({
+      include: [
+        {
+          model: User,
+        },
+      ],
+      order: [
+        ["date_created", "DESC"],
+        ["id", "DESC"],
+      ],
+    });
+
+    const posts = data.map((post) => post.get({ plain: true }));
+
     res.render("homepage", {
+      posts,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
